83d6f2341b70306800355402c7f48a321e4b6f8f
shiv12a10 <shiv12a10@gmail.com>
2024-02-21 22:22:49 +0530

    Initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+
+        <p className="text-xl mt-20">
+          Hello Guys, I am Shiv Pratap Singh from Gorakhpur, Uttar Pradesh. I expect you know Gorakhpur, haha that's funny.
+          So let me introduce more about myself. I am currently pursuing Bachelor Degree from Vellore Institute Of Technology, Bhopal
+          in Computer Science and Engineering with specialization in Gaming Technology. I have done my 10th and 12th from Metropolitan 
+          School (ICSE Board). I scored 85% in 10th and 84% in 12th. Currently I am learning Web Development and DSA.
+        </p>
+
+        <br />
+
+        <p className="text-xl">
+          My Hobbies are trading and Gaming. I love trading a lot. I am also a Intraday and 
+          Long Term investor. I love playing games with my friends.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
